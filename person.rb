663a1e49e9a16cf12aed9757a7e4d9a65e570e08
onesis <Onesis.olea@gmail.com>
2021-10-20 22:55:22 -0400

    Create person Class

---
+++ person.rb
@@ -0,0 +1,9 @@
+class Person
+  
+  def initialize(name: 'Unknown', age:, parent_permission: true)
+    @id = Random.rand(1..10000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+end

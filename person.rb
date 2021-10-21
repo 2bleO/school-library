8cac6ca3fbff5bdaf8c6aac61e90370d33387836
onesis <Onesis.olea@gmail.com>
2021-10-21 16:20:40 -0400

    Fix linter errors

--- person.rb
+++ person.rb
@@ -1,37 +1,19 @@
 require_relative 'corrector'
 
 class Person
-  
-  def initialize(name: 'Unknown', age:, parent_permission: true)
-    @id = Random.rand(1..10000)
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age:, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @corrector = Corrector.new
   end
 
-  def id
-    @id
-  end
-
-  def name
-    @name
-  end
-
-  def age
-    @age
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def age=(age)
-    @age = age
-  end
-
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
   def validate_name
@@ -39,8 +21,7 @@ class Person
   end
 
   private
-  def is_of_age?
-    @age>=18
+  def of_age?
+    @age >= 18
   end
-
 end

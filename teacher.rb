77173192e3a8656199d8fc53f6c11f43365f6b6f
onesis <Onesis.olea@gmail.com>
2021-10-20 23:25:45 -0400

    create teacher class

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require_relative 'person'
+
+class Teacher < Person
+  def initialize(age, specialization, name: 'Unknown')
+    super(name: name, age: age)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
